f7b7e7afad1e9958ef25304fe99213f9cc415300
RyoE46 <mido88ryo@gmail.com>
2022-11-10 11:17:40 +0900

    パスワード文字数

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, , format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }}
+
+  validates :nickname, presence: true
+  validates :password, format: { with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,100}+\z/i}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶヴー]+\z/}
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶヴー]+\z/}
+  validates :birthday, presence: true
+
+  has_many   :items
+  has_many   :orders
+
 end
