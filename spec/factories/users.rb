80761aeb675fe6620a2775c4c5a4b159caa06095
RyoE46 <mido88ryo@gmail.com>
2022-11-10 22:57:24 +0900

    gimei記入
    Failure/Error: require File.expand_path('../config/environment', __dir__)が発生して停まってる

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,12 +1,14 @@
 FactoryBot.define do
   factory :user do
+    japanese_user = Gimei.name
+
     nickname,           
     email,              { Faker::Internet.free_email }
     password,           
-    ltast_name,         
-    first_name,         
-    last_name_kana,     
-    first_name_kana,    
+    last_name,          { japanese_user.last.kanji }
+    first_name,         { japanese_user.first.kanji }
+    last_name_kana,     { japanese_user.last.katakana }
+    first_name_kana,    { japanese_user.first.katakana }
     birthday,           { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
   end
 end
