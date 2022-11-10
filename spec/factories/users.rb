084a34709d871aa8533e8f6007e55dc871991af7
RyoE46 <mido88ryo@gmail.com>
2022-11-10 23:36:34 +0900

    password_confirmation	 {password}追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,6 +5,7 @@ FactoryBot.define do
     nickname,           { japanese_user.kanji }
     email,              { Faker::Internet.free_email }
     password,           { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation	 {password}
     last_name,          { japanese_user.last.kanji }
     first_name,         { japanese_user.first.kanji }
     last_name_kana,     { japanese_user.last.katakana }
