90303340d981adacdc7394403f19fc6f23753f1e
RyoE46 <mido88ryo@gmail.com>
2022-11-11 12:49:36 +0900

    テストコード記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,14 +2,14 @@ FactoryBot.define do
   factory :user do
     japanese_user = Gimei.name
 
-    nickname,           { japanese_user.kanji }
-    email,              { Faker::Internet.free_email }
-    password,           { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
-    password_confirmation	 {password}
-    last_name,          { japanese_user.last.kanji }
-    first_name,         { japanese_user.first.kanji }
-    last_name_kana,     { japanese_user.last.katakana }
-    first_name_kana,    { japanese_user.first.katakana }
-    birthday,           { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
+    nickname              { japanese_user.kanji }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Lorem.characters(number: 6, min_alpha: 1, min_numeric: 1) }
+    password_confirmation { password }
+    last_name             { japanese_user.last.kanji }
+    first_name            { japanese_user.first.kanji }
+    last_name_kana        { japanese_user.last.katakana }
+    first_name_kana       { japanese_user.first.katakana }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
   end
 end
