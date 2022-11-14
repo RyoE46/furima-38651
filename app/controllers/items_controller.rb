7f5625293541cdeefecd7fc31cf15012057fad24
RyoE46 <mido88ryo@gmail.com>
2022-11-14 17:19:28 +0900

    ルーティングとコントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,21 @@
 class ItemsController < ApplicationController
-  def index
-  end
-def new
+  before_action :authenticate_user!, only: [:new]
 
+  def index
+  @items = Item.all
+  end
+
+def new
+  @item = Item.new
 end
 
 def create
-
+  @item = Item.new(item_params)
+  if @item.save
+    redirect_to root_path
+  else
+    render :new
+  end
 end
 
 def edit
