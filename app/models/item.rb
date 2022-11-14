b4e966007f086e2c044f5a551821567c268f0f5b
RyoE46 <mido88ryo@gmail.com>
2022-11-14 15:04:07 +0900

    hoge変更、ミス訂正

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,7 @@ class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 40 }
   validates :content, presence: true, length: { maximum: 1000 }
   validates :price, presence:true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9_999_999 }
-  validates :area_id, :category_id, :charge_id, :condition_id, :delivery_id, numericality: { other_than: 1 } 
+  validates :area_id, :category_id, :charge_id, :condition_id, :delivery_id,numericality: { other_than: 1 }
 
   has_one_attached :image
   belongs_to :user
