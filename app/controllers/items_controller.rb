9bd083d3ed93962dfdc8c4de3b0a8d7adf06463d
RyoE46 <mido88ryo@gmail.com>
2022-11-14 12:27:03 +0900

    コントローラーとルート定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
   end
+def new
+
+end
+
+def create
+
+end
+
+def edit
+
+end
+
+def show
+
+end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :content, :price, :image, :category_id, :condition_id, :charge_id, :area_id, :delivery_id).merge(user_id: current_user.id)
+  end
 end
