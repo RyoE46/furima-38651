41c705b7191600beb67f1516fcf1c0eb0b492eb5
RyoE46 <mido88ryo@gmail.com>
2022-11-15 21:49:53 +0900

    index,showアクション編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
-
-  def index
-    #@items = Item.all
-  end
+  before_action :set_item, only: [:show]
+def index
+  @items = Item.all
+end
 
 def new
   @item = Item.new
@@ -22,12 +22,16 @@ end
 
 #end
 
-#def show
-
-#end
+def show
+end
 
   private
   def item_params
     params.require(:item).permit(:name, :content, :price, :image, :category_id, :condition_id, :charge_id, :area_id, :delivery_id).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
 end
