25315367c34544cb3fb38e0ad94d23bf75af9cee
RyoE46 <mido88ryo@gmail.com>
2022-11-16 18:05:47 +0900

    HTML編集ルーティング直し
    ルーティングエラーで試行錯誤して結局htmlのform_withのmodelのインスタンス抜けだった

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [:show, :edit]
+  before_action :set_item, only: [:show, :edit, :update]
+  
 def index
   @items = Item.all.order("created_at DESC")
 end
@@ -19,8 +20,17 @@ def create
 end
 
 def edit
-
+  
 end
+
+def update
+  if @item.update(item_params)
+    redirect_to item_path(@item.id)
+  else
+    render :edit
+  end
+end
+
 
 def show
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,7 @@ class User < ApplicationRecord
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶヴー]+\z/}
   validates :birthday, presence: true
 
-  #has_many   :items
+  has_many   :items
   #has_many   :orders
 
 end
