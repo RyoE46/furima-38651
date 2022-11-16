82196dea04f87f1a0aa6ab310422b6d049834656
RyoE46 <mido88ryo@gmail.com>
2022-11-16 16:25:31 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
-  before_action :set_item, only: [:show]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:show, :edit]
 def index
   @items = Item.all.order("created_at DESC")
 end
@@ -18,9 +18,9 @@ def create
   end
 end
 
-#def edit
+def edit
 
-#end
+end
 
 def show
 end
